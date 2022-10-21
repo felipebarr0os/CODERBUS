1f3bb1093c34e78e72a8d5b343292e3407f0ec35
TaisC468 <112699437+TaisC468@users.noreply.github.com>
2022-10-21 02:17:43 -0300

    Update and rename serviço.js to servicos.js

---
+++ script/servicos.js
@@ -0,0 +1,12 @@
+document.getElementById("mybtn").addEventListener("click", function() {
+    alert("Obrigado por contratar nosso serviço!!");
+  });
+  document.getElementById("mybtn2").addEventListener("click", function() {
+    alert("Obrigado por contratar nosso serviço!!");
+  });
+  document.getElementById("mybtn3").addEventListener("click", function() {
+    alert("Obrigado por contratar nosso serviço!!");
+  });
+  document.getElementById("mybtn4").addEventListener("click", function() {
+    alert("Obrigado por contratar nosso serviço!!");
+  });
